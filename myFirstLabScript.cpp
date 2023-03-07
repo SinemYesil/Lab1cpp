22a424d60f19f1d11d2d04b9c059bf563416cb07
SinemYesil <yesil.sinem67@gmail.com>
2023-03-07 11:48:19 +0300

    Initial commit

---
+++ myFirstLabScript.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string name;
+    cout<<"What is your name?:";
+    cin>>name;
+    cout<<name<<endl;
+
+    cout<<"Hello "<<name<<endl;
+    cout<<"What is your Student ID?:";
+    int ID;
+    cin>>ID;
+    cout<<ID<<endl;
+    cout<<"Your ID is "<<ID<<endl;
+    return 0;
+
+}
+
+
